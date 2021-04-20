346f41d5cab769178688313658e6852b74bbb731
Krzysztof Czereczon <kczereczon@gmail.com>
2021-04-20 22:42:26 +0200

    Initial commit

--- src/main.js
+++ src/main.js
@@ -1,4 +1,31 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp, h } from 'vue'
+import './assets/tailwind.css'
+import LoginPage from "./pages/LoginPage"
+import HomePage from "./pages/HomePage"
 
-createApp(App).mount('#app')
+const NotFoundComponent = { template: '<p>Page not found</p>' }
+const AboutComponent = { template: '<p>About page</p>' }
+
+const routes = {
+  '/': HomePage,
+  '/about': AboutComponent,
+  '/login': LoginPage
+}
+
+const SimpleRouter = {
+  data: () => ({
+    currentRoute: window.location.pathname
+  }),
+
+  computed: {
+    CurrentComponent() {
+      return routes[this.currentRoute] || NotFoundComponent
+    }
+  },
+
+  render() {
+    return h(this.CurrentComponent)
+  }
+}
+
+createApp(SimpleRouter).mount('#app')
